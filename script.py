884c24c8f95cd312ca3aacb247006893e15f4d61
Jorge-Enriquez7 <69933840+Jorge-Enriquez7@users.noreply.github.com>
2020-10-13 20:58:29 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 #creacion de script
 from Bio.Seq import Seq
-from Bio.SeqFeatures import SeqFeatures
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 def summarize_contents(filename):
-	ejercicio = SeqIO.read("filenames", "genbank")
+	ejercicio = SeqIO.read(filenames, "genbank")
+	ejercicio_r = SeqRecord(ejercicio)
 	print(ejercicio)
+sunnarize_contents(filename)
