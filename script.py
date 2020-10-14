75edc956f8a6281013760f9e2664726809dfb7ac
Jorge-Enriquez7 <69933840+Jorge-Enriquez7@users.noreply.github.com>
2020-10-14 12:00:10 -0700

    Actualización

--- script.py
+++ script.py
@@ -6,12 +6,12 @@ from Bio.SeqRecord import SeqRecord
 import os
 def summarize_contents(filename):
 	SeqRec = SeqIO.read(filenames, "genbank")
-	print("Name: ", record.name)
+	print("Name: ", SeqRec.name)
 	print("Path: ",os.path.dirname(filename))
 	records = list(SeqIO.parse(filename,"genbank"))
 	print("Num_record = %i records" %len(records))
 	for seq_record in SeqIO.parse(filename,"genbank"):
-		print("ID :",record.id)
+		print("ID :",SeqRec.id)
 
 	
 sunnarize_contents(filename)
