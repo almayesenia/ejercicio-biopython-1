ec8655c2351c78272f9e01ba216af1a606bad01a
jorge-enriquez7 <jorgeed0307@gmail.com>
2020-10-13 19:37:39 -0700

    Correcion

---
+++ script.py
@@ -0,0 +1,7 @@
+from Bio.Seq import Seq
+from Bio.SeqFeatures import SeqFeatures
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+def summarize_contents(filename):
+	ejercicio = SeqIO.read("filenames", "genbank)
+	print(ejercicio)
