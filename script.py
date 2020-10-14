001740befec87420fea7f18d51557a301d822f1e
Jorge-Enriquez7 <69933840+Jorge-Enriquez7@users.noreply.github.com>
2020-10-14 15:04:51 -0700

    Actualización

--- script.py
+++ script.py
@@ -6,12 +6,17 @@ from Bio.SeqRecord import SeqRecord
 import os
 def summarize_contents(filename):
 	all_records = []
-	SeqRec = list(SeqIO.parse(filename,"genbank"))
+	record = list(SeqIO.parse(filename,"genbank"))
 	print("Path: ",os.path.dirname(filename))
-	print("Num_record = %i records" %len(SeqRec))
-	
-	for seq_record in SeqIO.parse(filename,"genbank"):
-		print("Name: ",seq_record.name)
-		print("ID :",SeqRec.id)
+	print("Num_record = %i records" %len(record))
+	print("\n")
+	for seq_r in SeqIO.parse(filename,"genbank"):
+		all_records.append(seq_r.name)
+		print("Name: ",seq_r.name)
+		print("ID :",seq_r.id)
+		print("Location:")
+		for seq_features in seq_r.features :
+			print('Star: %d, Stop: %d' %(int(seq_feature.location.start),int(seq_feature.location.end)))
+		
 	
 sunnarize_contents(filename)
